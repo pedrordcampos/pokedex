1a3d977a8bebe01f289dae1658a0115b5d55d48c
Pedro <pedro.rdcampos@hotmail.com>
2023-04-01 05:13:28 -0300

    Utilizando as promisses e melhorando as funções

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,23 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-fetch(url).then(function (response) {
-    console.log(response)
-});
+
+/*  Primeira forma
+    fetch(url)
+    .then(function (response) {
+        response.json().then(function (responseBody) {
+            console.log(responseBody);
+        });
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+    .finally(function (){
+        console.log('Requisição concluída!');
+    }); 
+*/
+
+fetch(url) // espera uma promisse
+    .then((response) => response.json()) // converte a promisse em um objeto json
+    .then((responseBody) => console.log(responseBody)) // permite manipular o objeto
+    .catch((error) => console.log(error))
